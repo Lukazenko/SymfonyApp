5853b34fb224c30b91325c808188e298ecee62d4
Lukazenko <veikko.kiis@khk.ee>
2019-11-27 11:59:08 +0200

    Added Delete and Update

---
+++ public/script.js
@@ -0,0 +1,19 @@
+
+// for delete controller
+
+const element = document.getElementById('tabel');
+
+if (element) {
+    element.addEventListener('click', e => {
+    if (e.target.className === 'btn btn-danger') {
+      if (confirm('Are you sure?')) {
+        const id = e.target.getAttribute('data-id');
+        
+
+        fetch(`./delete/${id}`, {
+          method: 'DELETE'
+        }).then(res => window.location.reload());
+      }
+    }
+  });
+}
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -30,7 +30,7 @@ class DefaultController extends AbstractController
     }
 
     /**
-     * @Route("/new", name="new contact")
+     * @Route("/new")
      * Method({"GET", "POST"})
      */
 
@@ -44,7 +44,7 @@ class DefaultController extends AbstractController
         $form = $this->createFormBuilder($contact)
             ->add('name', TextType::class, array('attr' => array('class' => 'form-control')))
             ->add('number', TextType::class, array('attr' => array('class' => 'form-control')))
-            ->add('save', SubmitType::class, array('attr' => array('class' => 'btn btn-primary')), ['label' => 'Create Contact'])
+            ->add('Add', SubmitType::class, array('attr' => array('class' => 'btn btn-primary')), ['label' => 'Create Contact'])
             ->getForm();
             
         $form->handleRequest($request);
@@ -61,8 +61,60 @@ class DefaultController extends AbstractController
         
 
         return $this->render('new.html.twig', [
-            'form' => $form->createView(),
+            'form' => $form->createView(), 'name' => 'New Contact'
         ]);
+
+        
+    }
+
+    /**
+     * @Route("/update/{id}")
+     * Method({"GET", "POST"})
+     */
+
+    public function update(Request $request, $id) {
+        $contact = new Contacts();
+        $contact = $this->getDoctrine()->getRepository(Contacts::class)->find($id);
+        
+
+        $form = $this->createFormBuilder($contact)
+            ->add('name', TextType::class, array('attr' => array('class' => 'form-control')))
+            ->add('number', TextType::class, array('attr' => array('class' => 'form-control')))
+            ->add('update', SubmitType::class, array('attr' => array('class' => 'btn btn-primary')), ['label' => 'Update'])
+            ->getForm();
+            
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            
+
+            $entityManager = $this->getDoctrine()->getManager();            
+            $entityManager->flush();
+
+            return $this->redirectToRoute('mainpage');
+        }
+        
+
+        return $this->render('new.html.twig', array(
+            'form' => $form->createView(), 'name' => 'Update'
+          ));
+      }
+
+    /**
+     * @Route("/delete/{id}")
+     * @Method({"DELETE"})
+     */
+
+    public function delete(Request $request, $id){
+
+        $contact = $this->getDoctrine()->getRepository(Contacts::class)->find($id);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($contact);
+        $entityManager->flush();
+
+        $response = new Response();
+        $response->send();
 
         
     }
